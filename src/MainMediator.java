42b1ffed9ec68a993a89e1f78cb683c6ed80a337
Sammuel13 <sammluna13@gmail.com>
2025-03-19 18:08:58 -0300

    modified main for new logic

---
+++ src/MainMediator.java
@@ -0,0 +1,46 @@
+import mediator.ATC;
+import mediator.ATCMediator;
+import mediator.Flight;
+import mediator.Runway;
+
+public class MainMediator {
+
+	public static void main(String[] args) {
+        ATCMediator atcMediator = new ATC();
+        
+        //Componente 1
+        Flight f1 = new Flight(atcMediator, "LATAM", "LA4542");
+            Flight f2 = new Flight(atcMediator, "GOL", "GL1273");
+        
+        //Componente 2
+        Runway mainRunway = new Runway(atcMediator);
+        
+        atcMediator.registerFlight(f1);
+        atcMediator.registerFlight(f2);
+
+        atcMediator.registerRunway(mainRunway);
+        
+        
+        System.out.println(">> Contato da aeronave 1....");
+        f1.getReady();
+        System.out.println();
+        
+        System.out.println(">> Consultando situacao da pista....");
+        mainRunway.land();
+        System.out.println();
+
+        System.out.println(">> Contato da aeronave 2....");
+        f2.getReady();
+        System.out.println();
+
+        System.out.println(">> Consultando situacao da pista....");
+        mainRunway.land();
+        
+        f1.land();
+
+        mainRunway.land();
+        f2.land();
+
+	}
+
+}
