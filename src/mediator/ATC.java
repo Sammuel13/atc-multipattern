44377f0b039e510cd84e040ec1dbf75e96654387
buenorafa <rafaelbuenooo@outlook.com>
2025-03-23 16:13:32 -0300

    adiciona fila de espera

--- src/mediator/ATC.java
+++ src/mediator/ATC.java
@@ -1,7 +1,9 @@
 package mediator;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class ATC implements ATCMediator {
 
@@ -15,6 +17,8 @@ public class ATC implements ATCMediator {
     private Runway runway;
     private SupportTeam supportTeam;
     public boolean land;
+
+    private Queue<Flight> waitingQueue = new LinkedList<>();
 
     @Override
     public void registerRunway(Runway runway) {
@@ -43,6 +47,7 @@ public class ATC implements ATCMediator {
     public void reserveRunway(Flight currentFlight) {
         if (this.currentFlight != null) {
             System.out.println("Pista ocupada.");
+            waitingQueue.add(currentFlight);
             return;
         }
         this.currentFlight = currentFlight;
@@ -53,5 +58,14 @@ public class ATC implements ATCMediator {
     @Override
     public void setLandingStatus(Integer status) {
         runway.setRunwayState(status);
+        if(status.equals(DISPONIVEL)){
+            this.currentFlight = null;
+
+            if(!waitingQueue.isEmpty()){
+                Flight nextFlight = waitingQueue.poll();
+                this.reserveRunway(nextFlight);
+//                nextFlight.land();
+            }
+        }
     }
 }
